95746148b5279a73391829562704179e3e8cb1ce
Joe Wasson <jwasson+git@gmail.com>
2010-08-04 23:34:11 -0700

    Change send troops to have sensible defaults.

--- sqwarea-helper.user.js
+++ sqwarea-helper.user.js
@@ -27,12 +27,110 @@ elmt.text = "(" + (function() {
     }
   }
 
+  function displayTroopsSender( data, troops ) {
+    if(troops == null) troops = 10;
+    if( data.kingId != Logic.king.id ) {
+        troops = Math.min(Logic.king.academyTroops,
+         data.troops == null ? (Logic.globals.initialSquareTroopsNumber + Logic.globals.minimumTroopsToClaimSquare)
+         : (data.troops + Logic.globals.minimumTroopsToClaimSquare) );
+    }
+    
+    $('.Actions .SendTroops.Pushed').click();
+    var o = this.displayMessage( 'Send troops', 
+        $('.Templates .SendTroopsDialog').clone(), 
+        { 
+            'Send' : function() {
+                var key = data.x + ',' + data.y;
+                data = Map._data[key];
+                if( (data.kingId != Logic.king.id) 
+                    && (data.troops > (troops - Logic.globals.minimumTroopsToClaimSquare)) ) {
+                    switch( Logic.globals.minimumTroopsToClaimSquare ) {
+                        case 0:
+                            if( !confirm( 'You need to have killed all troops'
+                                + ' at the end of the battle top claim square. Are you sure you want to continue ?' ) ) {
+                                return;
+                            }
+                            break;
+                        case 1:
+                            if( !confirm( 'You need to have 1 troop'
+                                + ' at the end of the battle top claim square. Are you sure you want to continue ?' ) ) {
+                                return;
+                            }
+                            break; 
+                        default:
+                            if( !confirm( 'You need to have ' + Logic.globals.minimumTroopsToClaimSquare + ' troops'
+                                + ' at the end of the battle top claim square. Are you sure you want to continue ?' ) ) {
+                                return;
+                            }
+                            break;
+                    }
+                }
+                $.post( '/Game/SendTroops', {
+                    x : data.x,
+                    y : data.y,
+                    sessionId : Logic.globals.sessionId,
+                    troops : troops
+                }, function( json ) {
+                    Logic.handleSynchronization( json );
+                    Map.handleSynchronization( json );
+                    if( !json.Success ) {
+                        Dialog.displaySimpleMessage( 'Movement failed',
+                            "<p>King can't send troops to this destination</p>" );
+                    }
+                }, 'json' );
+                Dialog.close( o );
+            },
+            
+            'Cancel' : function() {
+                Dialog.close( o );
+            }
+        } );
+     
+    $('.TroopsNumber', o).attr( 'value', troops );
+    $('.TroopsNumber', o).bind( 'change keyup blur', function() {
+        var value = $(this).attr( 'value' );
+        var flag = value;
+        value = value.replace( /[^0-9]/g, '' );
+        if( value != flag ) {
+            $(this).attr( 'value', value );
+        }
+        if( value == '' ) {
+            $('input[type=button]:first', o).attr( 'disabled', 'disabled' );
+            return;
+        }
+        value = parseInt(value);
+        if( value > Logic.king.academyTroops ) {
+            $('input[type=button]:first', o).attr( 'disabled', 'disabled' );
+            return;
+        }
+        $('input[type=button]:first', o).removeAttr( 'disabled' );
+        troops = value;
+        $('.Slider', o).slider( 'value', value );
+    } );
+        
+    $('.Slider', o).slider( {
+        min : 1,
+        value : troops,
+        max : Logic.king.academyTroops,
+        step : 1,
+        slide: function(e, ui) {
+            troops = ui.value;
+            $('.TroopsNumber', o).attr( 'value', ui.value );
+        }
+    } );
+    
+    $('input[type=text]', o).select();
+    return o;
+  }
+    
   function sqwarea_helper_run() {
+    Dialog.displayTroopsSender = displayTroopsSender;
     Map.square._base_initPoint = Map.square._initPoint;
     Map.square._base_mouseOver = Map.square._mouseOver;
     Map.square._initPoint = function(y, x, square) {
-      // Add coords info alert on ctrl+click
       Map.square._base_initPoint (y, x, square);
+
+      // Add coords info alert on ctrl+click
       square.bind ('click', function(e) {
         if (e.ctrlKey) {
           alert ("Square coords : (x, y) = (" + x + ", " + y + ")");
